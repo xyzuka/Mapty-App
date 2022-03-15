255d1004c92af2a7fe31f53a1821b3656ca8e5d4
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-15 15:06:15 +1100

    Rendering workout input form
    Cycling and running input event listeners added

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+// Creating a global variable for map and mapEvent
+let map, mapEvent;
+
 // Geolocation API
 navigator.geolocation.getCurrentPosition(
   function (position) {
@@ -20,7 +23,7 @@ navigator.geolocation.getCurrentPosition(
 
     const coords = [latitude, longitude];
 
-    const map = L.map("map").setView(coords, 13.5);
+    map = L.map("map").setView(coords, 13.5);
 
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -35,27 +38,49 @@ navigator.geolocation.getCurrentPosition(
           "pk.eyJ1IjoieHl6dWthIiwiYSI6ImNsMHJnMWZwajAzMmUzZHFwd2tpOHByeHkifQ.HOCAEgTtij9iRnK77Fn1BA",
       }
     ).addTo(map);
-      
-    // event listener for leaflet library
-    map.on('click', function(mapEvent) {
-      console.log(mapEvent);
-        const {lat, lng} = mapEvent.latlng;
-        
-        L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(L.popup({
-          maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup',
-        }))
-        .setPopupContent('Workout')
-        .openPopup();
 
-    })
+    // Handling clicks on map
+    map.on("click", function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove("hidden");
+      inputDistance.focus(); // focuses on the distance form as soon as the user clicks the map
+    });
   },
   function () {
     alert("Could not get your position");
   }
 );
+
+// Event listener: User pressing enter to submit form
+form.addEventListener("submit", function (e) {
+  // Prevents page from reloading when form is submitted
+  e.preventDefault();
+
+  // Resets input fields
+  form.reset();
+
+  // Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+// Event listener: Switches form input type when switching between running and cycling
+inputType.addEventListener('change', function() {
+  // toggling the hidden class on the .form__row parent of inputElevation and inputCadence
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
