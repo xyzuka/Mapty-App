2097ae010087f03eeea5debb9dd5ffa4ed0ba95d
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-15 16:29:31 +1100

    OOP refactoring
    Note: two methods had to use the .bind(this) method because the this keyword undefined when called. We need to manually bind the this keyword to its object

--- script.js
+++ script.js
@@ -14,73 +14,93 @@ const inputElevation = document.querySelector(".form__input--elevation");
 // Creating a global variable for map and mapEvent
 let map, mapEvent;
 
-// Geolocation API
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    // creating a variable out of the latitude property out of the coords object
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
+class App {
+  #map;
+  #mapEvent;
+  
+  constructor() {
+    this._getPosition();
 
-    const coords = [latitude, longitude];
+    // Event listener: User pressing enter to submit form to create a new workout
+    // Since the this keyword for _newWorkout will point to the form, we need to bind to the object itself 
+    form.addEventListener("submit", this._newWorkout.bind(this));
 
-    map = L.map("map").setView(coords, 13.5);
-
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoieHl6dWthIiwiYSI6ImNsMHJnMWZwajAzMmUzZHFwd2tpOHByeHkifQ.HOCAEgTtij9iRnK77Fn1BA",
-      }
-    ).addTo(map);
-
-    // Handling clicks on map
-    map.on("click", function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove("hidden");
-      inputDistance.focus(); // focuses on the distance form as soon as the user clicks the map
-    });
-  },
-  function () {
-    alert("Could not get your position");
+    // Event listener: Switches form input type when switching between running and cycling
+    inputType.addEventListener("change", this._toggleElevationField);
   }
-);
 
-// Event listener: User pressing enter to submit form
-form.addEventListener("submit", function (e) {
-  // Prevents page from reloading when form is submitted
-  e.preventDefault();
+  _getPosition() {
+    // Geolocation API
+    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+        alert("Could not get your position");
+      }
+    );
+  }
 
-  // Resets input fields
-  form.reset();
+  _loadMap(position) {
+      // creating a variable out of the latitude property out of the coords object
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
 
-  // Display marker
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
+      const coords = [latitude, longitude];
 
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("Workout")
-    .openPopup();
-});
+      this.#map = L.map("map").setView(coords, 13.5);
 
-// Event listener: Switches form input type when switching between running and cycling
-inputType.addEventListener('change', function() {
-  // toggling the hidden class on the .form__row parent of inputElevation and inputCadence
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken:
+            "pk.eyJ1IjoieHl6dWthIiwiYSI6ImNsMHJnMWZwajAzMmUzZHFwd2tpOHByeHkifQ.HOCAEgTtij9iRnK77Fn1BA",
+        }
+      ).addTo(this.#map);
+
+      // Handling clicks on map
+      this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus(); // focuses on the distance form as soon as the user clicks the map
+  }
+
+  _toggleElevationField() {
+          // toggling the hidden class on the .form__row parent of inputElevation and inputCadence
+          inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+          inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(e) {
+    // Prevents page from reloading when form is submitted
+    e.preventDefault();
+
+    // Resets input fields
+    form.reset();
+
+    // Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();   
+    }
+}
+
+// Creating a new object to trigger the constructor function and _getPosition() and _loadMap(position)
+const app = new App();
