569e70ca18ecf85d497a8deba1096f3936426430
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-15 17:40:22 +1100

    Creating Classes

--- script.js
+++ script.js
@@ -14,6 +14,50 @@ const inputElevation = document.querySelector(".form__input--elevation");
 // Creating a global variable for map and mapEvent
 let map, mapEvent;
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevation = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/hr
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5, 24, 188) 
+const cycle1 = new Cycling([39, -12], 38, 90, 523)
+console.log(run1, cycle1);
+
+// ******************* APPLICATION ARCHITECTURE ******************* //
 class App {
   #map;
   #mapEvent;
